91360f4a9f9d5610aa41da127643ca47d83acb1f
osafin <lamarLich@gmail.com>
2016-07-12 20:43:17 +0600

    поправил косяк от незакомиченных изменений, сорян

--- application/controllers/checkIP.php
+++ application/controllers/checkIP.php
@@ -90,6 +90,7 @@ class CheckIP extends CI_Controller {
 					{
 						$history=$history."<br>(5) N&gt;min &gt;sec city=Y good";
 						//$this->click_model->AddTimeOut(time());
+						$this->ip_model->InsertHistory($ip,$history);
 						return;
 					}
 					else
@@ -173,7 +174,9 @@ class CheckIP extends CI_Controller {
 				if (in_array($clientRegion, $ourRegion))
 				{
 					$history=$history."<br>(1) Y&gt;sec city=Y GOOD";
+
 					$this->ip_model->insert_ip($ip);
+					$this->ip_model->InsertHistory($ip,$history);
 					return;
 				}
 				else
