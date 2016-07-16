94c636f92661beb04edf3ca8c333c175d041eb11
vsb95 <vsb95@mail.ru>
2016-07-16 18:15:13 +0600

    Вывод без дубляжа ip + вывод ip только относящихся к выбранному сайту

--- application/views/panel_stat.php
+++ application/views/panel_stat.php
@@ -4,24 +4,47 @@ td {
 }
 </style>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<a href="http://landofbrand.ru/panel/regions">Выставить регионы</a>
 	<h3>Выберите сайты:</h3>
     <?php foreach ($all_sites as $site):?>
     	<input type="checkbox" name='site-<?=$site['id'];?>'>  <?=$site['name'];?><br>
     <?php endforeach;?>
 	<h3>Таблица плохих адресов</h3>
     <table>
-    <?php foreach($bad_IP as $ip):?> 
-		<tr>
-			<td><?=$ip['IP'];?></td>
-		</tr>
+    <?php foreach($clicks_bad_ip as $item):?>
+		<?php 
+			$arr[$item['id_Site']][] = $item['IP'];
+		?> 	
+	<?php endforeach;?>	
+	<?php foreach($arr as $key =>$item):?>
+		<?php 
+		$item=array_unique($item);
+			foreach($item as $value)
+			{
+				echo "<tr class='site site-$key'>";
+				echo "<td>$value</td>";
+				echo "</tr>";
+			}
+		?> 	
 	<?php endforeach;?>	
     </table>
     <h3>Таблица всех адресов</h3>
     <table>
-    <?php foreach($all_IP as $ip):?> 
-		<tr>
-			<td><?=$ip['IP'];?></td>
-		</tr>
+	<?php foreach($all_clicks as $item):?>
+		<?php 
+			$arra[$item['id_Site']][] = $item['IP'];
+		?> 	
+	<?php endforeach;?>	
+	<?php foreach($arra as $key =>$item):?>
+		<?php 
+		$item=array_unique($item);
+			foreach($item as $value)
+			{
+				echo "<tr class='site site-$key'>";
+				echo "<td>$value</td>";
+				echo "</tr>";
+			}
+		?> 	
 	<?php endforeach;?>	
     </table>
     
